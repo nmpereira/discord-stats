f7fdb850c34a8d7285a97eb8ca444a0c358eb7bf
nmpereira <nikhilpereira25@gmail.com>
2022-08-17 20:31:58 -0400

    mroe changes

--- app/server/index.js
+++ app/server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || randPort(3005, 3009);
 const app = express();
 const dotenv = require("dotenv").config();
 const fs = require("fs");
@@ -17,6 +17,9 @@ db.once("open", () => console.error("Connected to db"));
 app.listen(port, () => console.log(`The app is running on port ${port}!`));
 console.log("Env:", NODE_ENV);
 
+function randPort(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 const getUsersDb = async () => {
   return await UserModel.find().lean();
 };
@@ -116,8 +119,8 @@ async function urlCaller(url, arg1) {
 }
 
 const runner = async () => {
-  let users = [];
-  let users_db = [];
+  // let users = [];
+  // let users_db = [];
   let _lastid = 0;
   let total;
   let userBool = true;
@@ -129,8 +132,9 @@ const runner = async () => {
   let run_get_members = false;
   let run_get_messages_by_user = true;
   let start_time = Date.now();
+  let dbwriteCounter = 0;
   const time_since_start = (time) => {
-    return ((Date.now() - time) / 1000).toFixed(2);
+    return secondsToHMS(((Date.now() - time) / 1000).toFixed(2));
   };
 
   await urlCaller(get_user_count, "").then(({ data, status }) => {
@@ -142,22 +146,17 @@ const runner = async () => {
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
-  iterations = 5; //if override,specify. Default 'iterations' variable
+  iterations = iterations; //if override,specify. Default 'iterations' variable
   if (run_get_members) {
     for (let i = 0; i < iterations; i++) {
+      let users_db = [];
+      let iter_time = Date.now();
       await sleep(time_between_requests);
       const { data, status } = await urlCaller(
         get_users_in_guild,
         `?limit=${_limit}&after=${_lastid}`
       );
       users_found_counter += data.length;
-      console.log(
-        `Number of users found: ${
-          data.length
-        }|Total Found: ${users_found_counter} | Time Elapsed: ${time_since_start(
-          start_time
-        )}`
-      );
       if (data.length == 0) {
         userBool = false;
         return;
@@ -169,7 +168,7 @@ const runner = async () => {
         delete data_user["id"];
         data_user["user_count"] = user_counter;
         // console.log("user_counter", data_user);
-        users.push(data_user);
+        // users.push(data_user);
         users_db.push({
           updateOne: {
             filter: { disc_id: data_user.disc_id },
@@ -182,10 +181,19 @@ const runner = async () => {
       });
       try {
         await UserModel.collection.bulkWrite(users_db);
+
         // const writeDb = await User.insertMany();
       } catch (err) {
         console.log("db error:", err);
       }
+      console.log(
+        `Number of users found: ${
+          data.length
+        }|Total Found: ${users_found_counter} | Time Elapsed: ${time_since_start(
+          start_time
+        )} | Iteration time: ${time_since_start(iter_time)}`
+      );
+      iter_time = 0;
     }
     // console.log("users", users);
     // const User = new UserModel({
@@ -194,9 +202,25 @@ const runner = async () => {
     console.log("donezo getting users!");
   }
   if (run_get_messages_by_user) {
+    let rateLimitTimeout = 301000;
+    let rateLimitCounter = 0;
+    let rateLimitSuppress = 0;
+    time_between_requests = 1500;
+    let startOnUser = 20000; //default 0
+    let startAfterUser_bool = false;
     let Get_Message_start_time = Date.now();
     const users = await getUsersDb();
     for (let i = 0; i < users.length; i++) {
+      if (startOnUser != 0 && !startAfterUser_bool) {
+        i = startOnUser - 1;
+        console.log("startOnUser", startOnUser, startAfterUser_bool);
+        startAfterUser_bool = true;
+      }
+
+      let users_db = [];
+
+      let iter_time_2 = Date.now();
+
       const user = users[i];
       const disc_id = user.disc_id;
 
@@ -209,6 +233,57 @@ const runner = async () => {
           get_messages_by_user,
           `/search?author_id=${disc_id}`
         );
+        rateLimitSuppress++;
+        if (rateLimitSuppress >= 45) {
+          console.log(
+            `Suppressing rate limit after ${rateLimitSuppress} requests. Waiting ${secondsToHMS(
+              rateLimitTimeout / 1000
+            )}`
+          );
+          await sleep(rateLimitTimeout);
+          rateLimitSuppress = 0;
+          console.log("Continuing");
+        }
+
+        if (status >= 300) {
+          count_failure++;
+          console.log("Error message:", status, data.message);
+          if (status == 429) {
+            rateLimitCounter++;
+            console.log(
+              "Rate limit error:",
+              data.message,
+              "Code:",
+              status,
+              "retry after:",
+              data.retry_after
+            );
+            i = i - 1;
+            if (rateLimitCounter >= 3) {
+              console.log(
+                `Rate limited ${rateLimitCounter} times, waiting for ${rateLimitTimeout} ms before continuing `
+              );
+              await sleep(rateLimitTimeout);
+              console.log("continuing");
+              rateLimitCounter = 0;
+              rateLimitSuppress = 0;
+            } else {
+              console.log(`Rate limited ${rateLimitCounter} times`);
+              const retry_ms = Math.ceil(data.retry_after + 1) * 1000;
+              await sleep(retry_ms);
+            }
+          } else {
+            // on any other error, log the error and continue processing other users
+            console.log(
+              "Error message (Non 429 code):",
+              status,
+              "message:",
+              data.message
+            );
+          }
+
+          continue;
+        }
         // update the db with this
         const message_obj = {
           disc_id: disc_id,
@@ -216,15 +291,6 @@ const runner = async () => {
           messages: data.messages,
         };
 
-        console.log(
-          `Number of messages for ${message_obj.disc_id}:${
-            message_obj.total_messages
-          }. Time Elapsed: ${time_since_start(
-            start_time
-          )} | Time Elapsed (Message Count): ${time_since_start(
-            Get_Message_start_time
-          )}`
-        );
         users_db.push({
           updateOne: {
             filter: { disc_id: message_obj.disc_id },
@@ -241,32 +307,17 @@ const runner = async () => {
           console.log("db error:", err);
         }
 
-        if (status >= 300) {
-          count_failure++;
-          console.log("Error message:", status, data.message);
-          if (status == 429) {
-            console.log(
-              "Rate limit error:",
-              data.message,
-              "Code:",
-              status,
-              "retry after:",
-              data.retry_after
-            );
-            i = i - 1;
-            const retry_ms = Math.ceil(data.retry_after + 1) * 1000;
-            await sleep(retry_ms);
-          } else {
-            console.log(
-              "Error message (Non 429 code):",
-              status,
-              "message:",
-              data.message
-            );
-          }
-
-          continue;
-        }
+        console.log(
+          `Msgs for (${user.user_count}) ${message_obj.disc_id}:${
+            message_obj.total_messages
+          }. Elapsed (total): ${time_since_start(
+            start_time
+          )} | Elapsed: ${time_since_start(
+            Get_Message_start_time
+          )} | Iter time: ${time_since_start(
+            iter_time_2
+          )} | Iter before suppress ${rateLimitSuppress}`
+        );
         count_failure = 0;
       } catch (err) {
         console.log("err:", err);
@@ -274,8 +325,21 @@ const runner = async () => {
         count_failure++;
         i = i - 1;
       }
+      iter_time_2 = 0;
     }
     console.log("donezo getting messages!");
   }
 };
 runner();
+const secondsToHMS = (secs) => {
+  var sec_num = parseInt(secs, 10);
+  var hours = Math.floor(sec_num / 3600);
+  var minutes = Math.floor(sec_num / 60) % 60;
+  var seconds = sec_num % 60;
+  if (secs > 0) {
+    return [hours, minutes, seconds]
+      .map((v) => (v < 10 ? "0" + v : v))
+      .filter((v, i) => v !== "00" || i > 0)
+      .join(":");
+  } else return 0;
+};
